94633a688a0e1ee0f8a42d4df5fd2c5f4827788e
Birdy87 <birdfire@hotmail.fr>
2021-01-13 16:08:21 +0100

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,72 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonList, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent } from '@ionic/react';
+import { add } from 'ionicons/icons'
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+ // import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    <IonContent>
+    {/*-- List of Text Items --*/}
+    <IonList>
+      <IonItem>
+        <IonLabel>Pokémon Yellow</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Mega Man X</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>The Legend of Zelda</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Pac-Man</IonLabel>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Super Mario World</IonLabel>
+      </IonItem>
+    </IonList>
+
+    {/*-- List of Input Items --*/}
+    <IonList>
+      <IonItem>
+        <IonLabel>Input</IonLabel>
+        <IonInput></IonInput>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Toggle</IonLabel>
+        <IonToggle slot="end"></IonToggle>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Radio</IonLabel>
+        <IonRadio slot="end"></IonRadio>
+      </IonItem>
+      <IonItem>
+        <IonLabel>Checkbox</IonLabel>
+        <IonCheckbox slot="start" />
+      </IonItem>
+    </IonList>
+
+    {/*-- List of Sliding Items --*/}
+    <IonList>
+      <IonItemSliding>
+        <IonItem>
+          <IonLabel>Item</IonLabel>
+        </IonItem>
+        <IonItemOptions side="end">
+          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+
+      <IonItemSliding>
+        <IonItem>
+          <IonLabel>Item</IonLabel>
+        </IonItem>
+        <IonItemOptions side="end">
+          <IonItemOption onClick={() => {}}>Unread</IonItemOption>
+        </IonItemOptions>
+      </IonItemSliding>
+    </IonList>
+  </IonContent>
   );
 };
 
